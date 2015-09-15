126b5e860ae023cb1ae4456a84b8f2ad86277cbd
Evan Lin <evanslin@gmail.com>
2015-09-15 17:37:18 +0800

    add travis

--- dfa.go
+++ dfa.go
@@ -1,13 +1,34 @@
 package dfa
 
+type transitionInput struct {
+	srcState int
+	input    interface{}
+}
+
 type dfa struct {
 	initState    int
 	currentState int
 	totalStates  []int
 	finalStates  []int
-	transition   map[int]int
+	transition   map[transitionInput]int
 }
 
 func NewDFA() *dfa {
-	return &dfa{}
+	return &dfa{transition: make(map[transitionInput]int, 0)}
+}
+
+func (d *dfa) AddState(state int, isFinal bool) {
+	d.totalStates = append(d.totalStates, state)
+	if isFinal {
+		d.finalStates = append(d.finalStates, state)
+	}
+}
+
+func (d *dfa) AddTransition(srcState int, input interface{}, dstStateint int) {
+	targetTrans := transitionInput{srcState: srcState, input: input}
+	d.transition[targetTrans] = dstStateint
+}
+
+func (d *dfa) Input(testInput interface{}) bool {
+	return false
 }
